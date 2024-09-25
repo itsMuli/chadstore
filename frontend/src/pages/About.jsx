af8569d08c549c109ad8c8e6df7c2aa8cfa89a50
itsMuli <rosemwendwa163@gmail.com>
2024-09-25 12:45:58 +0300

    frontend

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,42 @@
-
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quam molestias debitis illo illum labore, quisquam voluptas nemo laudantium ipsa quas, minima qui maxime, libero iste officia unde delectus. Provident.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, aliquid vero ipsam, nisi, dolores atque repellat blanditiis a facere ducimus eligendi dolorem accusamus. Assumenda error impedit cumque voluptates consectetur explicabo.</p>
+        <b className='text-gray-800'>Our Mission</b>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati maiores possimus error ipsa quidem voluptas maxime ad voluptatum totam natus quia quasi facilis, expedita accusantium dolorum numquam, doloribus atque earum!</p>
+        </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, aliquam repellat quas earum beatae, unde aspernatur deserunt neque dolorum mollitia voluptate in omnis odit distinctio quae. Nam quasi recusandae exercitationem.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convinience:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, aliquam repellat quas earum beatae, unde aspernatur deserunt neque dolorum mollitia voluptate in omnis odit distinctio quae. Nam quasi recusandae exercitationem.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional customer Service:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, aliquam repellat quas earum beatae, unde aspernatur deserunt neque dolorum mollitia voluptate in omnis odit distinctio quae. Nam quasi recusandae exercitationem.</p>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [currentState, setCurrentState] = useState("Sign Up");
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  }
 
-export default Login
+  return (
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? null : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-600"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-600"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-600"
+        placeholder="Password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm">
+        <p className="cursor-pointer">Forgot your Password?</p>
+        {currentState === "Login" ? (
+          <p onClick={() => setCurrentState("Sign Up")} className="cursor-pointer">
+            Create account
+          </p>
+        ) : (
+          <p onClick={() => setCurrentState("Login")} className="cursor-pointer">
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4 hover:bg-gray-700 transition-colors duration-300 ease-in-out">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
+};
+
+export default Login;
