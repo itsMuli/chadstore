7d3dd45608a8b46ae056e3d370fac3e9fe4f22a5
itsMuli <rosemwendwa163@gmail.com>
2024-10-14 13:08:18 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,26 @@ import productRouter from './routes/productRoutes.js';
 import cartRouter from './routes/cartRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
+
 connectDB();
-connectCloudinary
+connectCloudinary();  
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/user', userRouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
-app.get('/', (req,res)=>{
-    res.send("API working")
-})
+app.get('/', (req, res) => {
+    res.send('API working');
+});
 
-app.listen(port, ()=> console.log('Server running on PORT : '+ port))
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(port, () => console.log('Server running on PORT : ' + port));
